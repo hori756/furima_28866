47cf05dbec8efcdcca940ed2ffdee701a13f68ab
hori756 <horikoshi756@gmail.com>
2020-08-28 16:01:39 +0900

    Faker導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"nago"}
-    email                 {"sample755@gmail.com"}
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
     password              {"fff666"}
     password_confirmation {password}
     first_name            {"太郎"}
