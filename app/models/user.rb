5bdc126bb73811be8eb37b73056b8db5bf64b9c0
hori756 <horikoshi756@gmail.com>
2020-08-28 16:41:17 +0900

    Rubocopを実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,22 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
-  validates :email, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}, uniqueness: {case_sensitive: false}
-  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i},length: {minimum: 6}
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: { case_sensitive: false }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }, length: { minimum: 6 }
   with_options presence: true do
     validates :nickname
     validates :birth_date
 
-    with_options format: {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/} do
+    with_options format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ } do
       validates :first_name
       validates :family_name
     end
 
-    with_options format: {with: /\A[ァ-ヶー－]+\z/} do
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
       validates :first_name_kana
       validates :family_name_kana
     end
   end
-
 end
