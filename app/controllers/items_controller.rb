46c7dd7f0e5214b08deb307801fc433c2ea620fd
hori756 <horikoshi756@gmail.com>
2020-08-31 11:21:13 +0900

    ログインしてないとログイン画面へ移動

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,17 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+  
   def index
   end
+
   def new
+  end
+
+  private
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to user_session_path
+    end
   end
 end
