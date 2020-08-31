5736e121971afb514550e86c7ba62a69a201e894
hori756 <horikoshi756@gmail.com>
2020-08-31 16:00:37 +0900

    itemマイグレーション設定

--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2020_08_31_023231) do
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "user", null: false
+    t.integer "user_id", null: false
     t.string "name", null: false
     t.text "description", null: false
     t.integer "category_id", null: false
