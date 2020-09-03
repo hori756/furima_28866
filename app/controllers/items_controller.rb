e4f2e421b9a0df03115d2b1e15959737d782419f
hori756 <horikoshi756@gmail.com>
2020-09-03 11:22:23 +0900

    showコントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
+  before_action :set_item, only: [:show, :destroy, :edit, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -22,21 +23,24 @@ class ItemsController < ApplicationController
 
   def move_to_index
     redirect_to user_session_path unless user_signed_in?
+    end
   end
-end
 
-def item_params
-  params.require(:item)
-        .permit(
-          :name,
-          :description,
-          :category_id,
-          :status_id,
-          :postage_payer_id,
-          :prefecture_id,
-          :shipping_day_id,
-          :price,
-          :images
-        )
-        .merge(user_id: current_user.id)
-end
+  def item_params
+    params.require(:item)
+          .permit(
+            :name,
+            :description,
+            :category_id,
+            :status_id,
+            :postage_payer_id,
+            :prefecture_id,
+            :shipping_day_id,
+            :price,
+            :images
+          )
+          .merge(user_id: current_user.id)
+  end
+  def set_item
+    @item = Item.find(params[:id])
+  end
