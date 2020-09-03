a15ef325538716f1dc8359c64c6f557afd86654d
hori756 <horikoshi756@gmail.com>
2020-09-03 12:09:04 +0900

    不要なアクション削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
-  before_action :set_item, only: [:show, :destroy, :edit, :update]
+  before_action :set_item, only: [:show]
 
   def index
     @items = Item.all.order("created_at DESC")
