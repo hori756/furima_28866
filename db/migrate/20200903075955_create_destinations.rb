1de1c9500f6f8585efc14aab0d2be0a32dc12ff1
hori756 <horikoshi756@gmail.com>
2020-09-03 17:09:21 +0900

    destinationsテーブル作成

--- db/migrate/20200903075955_create_destinations.rb
+++ db/migrate/20200903075955_create_destinations.rb
@@ -1,7 +1,13 @@
 class CreateDestinations < ActiveRecord::Migration[6.0]
   def change
     create_table :destinations do |t|
-
+      t.references :purchase,                     null: false,  foreign_key: true
+      t.string :post_code,    default: "",  null: false
+      t.integer :prefecture_id,                  null: false
+      t.string :city,           default: "", null: false
+      t.string :adress,  default: "", null: false
+      t.string :build_name,  default: ""
+      t.string :phone_number,   default: "", null: false
       t.timestamps
     end
   end
