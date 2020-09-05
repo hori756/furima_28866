8443565e984bcc9ce73a46f3b78bddfbeffae444
hori756 <horikoshi756@gmail.com>
2020-09-05 23:32:32 +0900

    adddressカラム作成2

--- db/migrate/20200903075955_create_destinations.rb
+++ db/migrate/20200903075955_create_destinations.rb
@@ -5,7 +5,7 @@ class CreateDestinations < ActiveRecord::Migration[6.0]
       t.string :post_code,    default: "",  null: false
       t.integer :prefecture_id,                  null: false
       t.string :city,           default: "", null: false
-      t.string :adress,  default: "", null: false
+      t.string :address,  default: "", null: false
       t.string :build_name,  default: ""
       t.string :phone_number,   default: "", null: false
       t.timestamps
