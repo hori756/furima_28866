d58ac312a9fde8c4ae1888260ffbd9dbb2d70d0c
hori756 <horikoshi756@gmail.com>
2020-09-05 23:30:06 +0900

    purchaseコントローラー作成

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,43 @@
 class PurchasesController < ApplicationController
+  def index
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @purchase = PurchaseDestination.new(purchase_params)
+    if @purchase.valid?
+      pay_item
+      @purchase.save
+      return redirect_to root_path(@item.user_id)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def pay_item
+    Payjp.api_key = "sk_test_1d5eb400ef09731178f12296"  # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: purchase_params[:token],    # カードトークン
+      currency:'jpy'                 # 通貨の種類(日本円)
+    )
+  end
+
+  def purchase_params
+    params.permit(
+            :item_id,
+            :purchase_id,
+            :token,
+            :post_code,
+            :prefecture_id,
+            :city,
+            :address,
+            :build_name,
+            :phone_number,
+          )
+          .merge(user_id: current_user.id)
+  end
 end
